6165e0573e54babbdd9a31e97bb892a2709e2df0
Alex Lohr <alexthkloss@web.de>
2026-04-20 15:26:24 +0200

    fix: coverage, vitest setup

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,13 +5,14 @@ export default defineConfig({
   plugins: [solidPlugin()],
   test: {
     coverage: {
+      enabled: true,
       reporter: ["lcov", "text"],
-      include: ["src/index.ts"],
+      include: ["src/index.*"],
       exclude: ["src/types.ts"],
     },
     watch: false,
     globals: true,
     clearMocks: true,
-    include: "src/__tests__/*.tsx"
+    include: ["src/__tests__/*.tsx"]
   },
 });
